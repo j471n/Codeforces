d1426baf2fe2ed99d558763500f519251186560d
Jatin Sharma <jatinsharma089659@gmail.com>
2020-09-20 19:54:07 +0530

    Adding "4A" Solution in C

---
+++ ProblemSet/4A_Watermelon/C/Solution.c
@@ -0,0 +1,16 @@
+# Programmer : Jatin Sharma
+
+#include<stdio.h>
+ 
+int main(){
+	int s;
+	scanf("%d", &s);
+	if (s%2==0 && s>2){
+		printf("YES");
+	}
+	else {
+		printf("NO");
+	}
+ 
+	return 0;
+}
