6098dc2f3e352627979b3c44500c41716aa7f22f
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-09-23 15:46:11 +0530

    Adding '112A' Solution in Python, C, C++

---
+++ ProblemSet/112A_Petya and Strings/C/solution.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str1[100];
+    char str2[100];
+    gets(str1);
+    gets(str2);
+
+    int result = stricmp(str1, str2);
+
+    if (result > 0)
+        printf("1\n");
+
+    else if (result < 0)
+        printf("-1\n");
+
+    else
+        printf("0\n");
+
+    return (0);
+}
