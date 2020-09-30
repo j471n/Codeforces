9d90edb1107243bf9fb890ef8f0dda7077537e7e
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-09-30 14:49:00 +0530

    Adding "344A" in Python, C, C++
    Magnets

---
+++ ProblemSet/344A_Magnets/Python/solution.py
@@ -0,0 +1,11 @@
+# Date : 30-Sep-2020 By Jatin Sharma
+
+n = int(input())
+
+last_magnet = "00"
+count = 0
+for i in range(n):
+    a = input()
+    if a != last_magnet: count += 1
+    last_magnet = a
+print(count)
