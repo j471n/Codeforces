48d058facf2ec8e6e7d4cfa71e38be591c7a6f91
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-09-30 15:44:32 +0530

    Adding "486A" in Python, C, C++
    Calculation Function

---
+++ ProblemSet/486A_Calculating Function/C/solution.c
@@ -0,0 +1,20 @@
+// Date : 30 - Sep - 2020 By Jatin Sharma
+
+#include <stdio.h>
+
+
+int main()
+{
+    long long int n, ans;
+    scanf("%lld", &n);
+    if (n % 2 == 0)
+    {
+        ans = n / 2;
+    }
+    if (n % 2 == 1)
+    {
+        ans = (n - 1) / 2 - n;
+    }
+    printf("%lld\n", ans);
+    return 0;
+}
---
+++ ProblemSet/486A_Calculating Function/Python/solution.py
@@ -0,0 +1,8 @@
+# Date : 30-Sep-2020 By Jatin Sharma
+
+
+from math import ceil
+
+n = int(input())
+
+print(ceil(n / 2) * ((-1)**n))
